57b4cce2ceb6ce69263e6d4db686b1ef1ed29543
aselvan8 <65464009+aselvan8@users.noreply.github.com>
2020-08-27 17:36:07 -0400

    solved UFO-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,15 +4,11 @@ var tableData = data;
 // Select the button
 var button = d3.select("#filter-btn");
 
-// for input values
+// Select the form
+var form = d3.select("form");
+
+// to append table in html
 var tbody = d3.select("tbody");
-
-// // Select the form
-// var form = d3.select("#form");
-
-// Create event handlers 
-button.on("click", runEnter);
-// form.on("submit",runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -22,23 +18,25 @@ function runEnter() {
 
     // Select the input element and get the raw HTML node and Get the value property of the input element
     var inputDateValue = d3.select("#datetime").property("value");
-    var inputCityValue = d3.select("#city").property("value");
-    var inputStateValue = d3.select("#state").property("value");
-    var inputCountryValue = d3.select("#country").property("value");
-    var inputShapeValue = d3.select("#shape").property("value");
+
+    // var inputCityValue = d3.select("#city").property("value");
+    // var inputStateValue = d3.select("#state").property("value");
+    // var inputCountryValue = d3.select("#country").property("value");
+    // var inputShapeValue = d3.select("#shape").property("value");
+
     // var inputCommentValue = d3.select("#comment").property("value");
 
-    console.log("inputdatevalue: ", inputValue);
+    console.log("inputdatevalue: ", inputDateValue);
     console.log(tableData);
 
-    var filteredData = tableData.filter(ufoinfo => ufoinfo.datetime === inputValue);
+    var filteredData = tableData.filter(ufoinfo => ufoinfo.datetime === inputDateValue);
 
     console.log(filteredData);
 
     // remove info from table
     tbody.html("");
 
-    filterData.forEach((ufoinfo) => {
+    filteredData.forEach((ufoinfo) => {
         var row = tbody.append("tr");
         Object.entries(ufoinfo).forEach(([key, value]) => {
             var cell = row.append("td").text(value);
@@ -48,4 +46,6 @@ function runEnter() {
 
 };
 
-
+// Create event handlers
+button.on("click", runEnter);
+form.on("submit",runEnter);
